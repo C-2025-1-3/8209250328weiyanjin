e253d37876245a74bf8cf521a45308b4bcaebdad
b1ush <w18534752886@126.com>
2025-11-13 20:03:52 +0800

    fix:experiment4

--- experiment4.cpp
+++ experiment4.cpp
@@ -1,8 +1,26 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-double a,b,ans;
+double a, b, ans;
 char ch;
-int main(){
-    
-    return 0;
+int main() {
+  cin >> a >> ch >> b;
+  if (ch == '+') {
+    cout << "=" << a + b << "\n";
+  } else if (ch == '-') {
+    cout << "=" << a - b << "\n";
+  } else if (ch == '*') {
+    cout << "=" << a * b << "\n";
+  } else if (ch == '/') {
+    if (b == 0) {
+      cout << "除数不能为 0!\n";
+      return 0;
+    }
+    cout << "=" << a / b << endl;
+  } else if (ch == '%') {
+    int a1 = (int)a, b1 = (int)b;
+    cout << "=" << a1 % b1 << "\n";
+  } else {
+    cout << "非法运算符\n";
+  }
+  return 0;
 }
